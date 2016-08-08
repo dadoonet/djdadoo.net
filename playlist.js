647da869ab0de6b5d0d1b351cdeebc27b764dc15
David Pilato <david@pilato.fr>
2016-08-08 19:23:16 +0200

    Update to howler lib

---
+++ playlist.js
@@ -0,0 +1,94 @@
+var dadoonetPlayList = [
+  {
+    title: 'Pre elastic{ON} 16 Mix',
+    file: 'http://dj.david.pilato.fr/mixes/2016-02-12-Pre-ElasticON16-Mix.mp3',
+    howl: null
+  },
+  {
+    title: 'Fred Birthday Live Mix - Part 1',
+    file: 'http://dj.david.pilato.fr/mixes/2015-11-21-Fred-Birthday-Live-Mix-Part1.mp3',
+    howl: null
+  },
+  {
+    title: 'Fred Birthday Live Mix - Part 2',
+    file: 'http://dj.david.pilato.fr/mixes/2015-11-21-Fred-Birthday-Live-Mix-Part2.mp3',
+    howl: null
+  },
+  {
+    title: 'Pre elastic{ON} 15 Mix',
+    file: 'http://dj.david.pilato.fr/mixes/2015-01-18-pre-elasticon.mp3',
+    howl: null
+  },
+  {
+    title: 'October Peniche Mix - Diner',
+    file: 'http://dj.david.pilato.fr/mixes/2013-10-05-Peniche-Diner.mp3',
+    howl: null
+  },
+  {
+    title: 'October Peniche Mix - Dance Part 1 (80s)',
+    file: 'http://dj.david.pilato.fr/mixes/2013-10-05-Peniche-80.mp3',
+    howl: null
+  },
+  {
+    title: 'October Peniche Mix - Dance Part 2 (Club)',
+    file: 'http://dj.david.pilato.fr/mixes/2013-10-05-Peniche-Dance.mp3',
+    howl: null
+  },
+  {
+    title: 'Anne s Birthday December Live Mix - Part 1',
+    file: 'http://dj.david.pilato.fr/mixes/09_DJ_Dadoo_Anne_Birthday-December-Part1.mp3',
+    howl: null
+  },
+  {
+    title: 'Anne s Birthday December Live Mix - Part 2',
+    file: 'http://dj.david.pilato.fr/mixes/09_DJ_Dadoo_Anne_Birthday-December-Part2.mp3',
+    howl: null
+  },
+  {
+    title: 'Summer Mix 2012',
+    file: 'http://dj.david.pilato.fr/mixes/08_DJ_Dadoo_Summer_Mix.mp3',
+    howl: null
+  },
+  {
+    title: 'July Live mix - Sebastien and Elvira Wedding - Part 1',
+    file: 'http://dj.david.pilato.fr/mixes/07_DJ_Dadoo_July-Mix-DDJT1-Part1.mp3',
+    howl: null
+  },
+  {
+    title: 'July Live mix - Sebastien and Elvira Wedding - Part 2',
+    file: 'http://dj.david.pilato.fr/mixes/07_DJ_Dadoo_July-Mix-DDJT1-Part2.mp3',
+    howl: null
+  },
+  {
+    title: 'May Mix 2012',
+    file: 'http://dj.david.pilato.fr/mixes/06_DJ_Dadoo_May-Mix-DDJT1.mp3',
+    howl: null
+  },
+  {
+    title: 'April Mix 2012',
+    file: 'http://dj.david.pilato.fr/mixes/05_DJ_Dadoo_April-Mix-DDJT1.mp3',
+    howl: null
+  },
+  {
+    title: 'March Mix 2012',
+    file: 'http://dj.david.pilato.fr/mixes/04_DJ_Dadoo_March-Mix-DDJT1.mp3',
+    howl: null
+  },
+  {
+    title: 'Club Mix 2012',
+    file: 'http://dj.david.pilato.fr/mixes/03_DJ_Dadoo_Club-Mix-DDJT1.mp3',
+    howl: null
+  },
+  {
+    title: 'Funk Groove Mix 2012',
+    file: 'http://dj.david.pilato.fr/mixes/02_DJ_Dadoo_First-Mix-DDJT1.mp3',
+    howl: null
+  },
+  {
+    title: 'Funk Mix 2011',
+    file: 'http://dj.david.pilato.fr/mixes/01_DJ_Dadoo_Funk_Mix.mp3',
+    howl: null
+  }
+];
+
+
