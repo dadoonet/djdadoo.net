7572de42bcdead8c12281a3a73a07c08e1ed8c87
David Pilato <david@pilato.fr>
2022-01-12 18:48:20 +0100

    Use Google Cloud Storage instead of Ionos

--- playlist.js
+++ playlist.js
@@ -1,131 +1,131 @@
 var dadoonetPlayList = [{
         title: '1er An - Lolo et Mika',
-        file: 'https://dj.david.pilato.fr/mixes/2020-01-01-LoloEtMika-1An-fullmix.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2020-01-01-LoloEtMika-1An-fullmix.mp3',
         howl: null
     },
     {
         title: 'Wedding Natacha - Warm Up',
-        file: 'https://dj.david.pilato.fr/mixes/2019-08-31-Wedding-Natacha-Warmup.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2019-08-31-Wedding-Natacha-Warmup.mp3',
         howl: null
     },
     {
         title: 'BreizhCamp 2018 - Meet And Greet Party Mix (with Julien Ponge and David Pilato)',
-        file: 'https://dj.david.pilato.fr/mixes/2018-03-29-breizhcamp-meet-and-greet.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2018-03-29-breizhcamp-meet-and-greet.mp3',
         howl: null
     },
     {
         title: 'BreizhCamp 2017 - Meet And Greet Party Mix',
-        file: 'https://dj.david.pilato.fr/mixes/2017-04-20-breizhcamp-meet-and-greet.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2017-04-20-breizhcamp-meet-and-greet.mp3',
         howl: null
     },
     {
         title: 'BreizhCamp 2017 - Keynote Mix',
-        file: 'https://dj.david.pilato.fr/mixes/2017-04-20-breizhcamp-keynote.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2017-04-20-breizhcamp-keynote.mp3',
         howl: null
     },
     {
         title: 'Winter 2017 Pre Elastic{ON} 17 Mix',
-        file: 'https://dj.david.pilato.fr/mixes/2017-03-05-Winter-2017-Pre-ElasticON-Mix.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2017-03-05-Winter-2017-Pre-ElasticON-Mix.mp3',
         howl: null
     },
     {
         title: 'Summer 2016 Garden Mix',
-        file: 'https://dj.david.pilato.fr/mixes/2016-08-28-Summer-2016-Garden-Mix.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2016-08-28-Summer-2016-Garden-Mix.mp3',
         howl: null
     },
     {
         title: 'Live - Fred Birthday',
-        file: 'https://dj.david.pilato.fr/mixes/2016-07-02-Fred-Birthday-Cergy.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2016-07-02-Fred-Birthday-Cergy.mp3',
         howl: null
     },
     {
         title: 'Pre elastic{ON} 16 Mix',
-        file: 'https://dj.david.pilato.fr/mixes/2016-02-12-Pre-ElasticON16-Mix.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2016-02-12-Pre-ElasticON16-Mix.mp3',
         howl: null
     },
     {
         title: 'Fred Birthday Live Mix - Part 1',
-        file: 'https://dj.david.pilato.fr/mixes/2015-11-21-Fred-Birthday-Live-Mix-Part1.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2015-11-21-Fred-Birthday-Live-Mix-Part1.mp3',
         howl: null
     },
     {
         title: 'Fred Birthday Live Mix - Part 2',
-        file: 'https://dj.david.pilato.fr/mixes/2015-11-21-Fred-Birthday-Live-Mix-Part2.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2015-11-21-Fred-Birthday-Live-Mix-Part2.mp3',
         howl: null
     },
     {
         title: 'Pre elastic{ON} 15 Mix',
-        file: 'https://dj.david.pilato.fr/mixes/2015-01-18-pre-elasticon.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2015-01-18-pre-elasticon.mp3',
         howl: null
     },
     {
         title: 'October Peniche Mix - Diner',
-        file: 'https://dj.david.pilato.fr/mixes/2013-10-05-Peniche-Diner.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2013-10-05-Peniche-Diner.mp3',
         howl: null
     },
     {
         title: 'October Peniche Mix - Dance Part 1 (80s)',
-        file: 'https://dj.david.pilato.fr/mixes/2013-10-05-Peniche-80.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2013-10-05-Peniche-80.mp3',
         howl: null
     },
     {
         title: 'October Peniche Mix - Dance Part 2 (Club)',
-        file: 'https://dj.david.pilato.fr/mixes/2013-10-05-Peniche-Dance.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/2013-10-05-Peniche-Dance.mp3',
         howl: null
     },
     {
         title: 'Anne s Birthday December Live Mix - Part 1',
-        file: 'https://dj.david.pilato.fr/mixes/09_DJ_Dadoo_Anne_Birthday-December-Part1.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/09_DJ_Dadoo_Anne_Birthday-December-Part1.mp3',
         howl: null
     },
     {
         title: 'Anne s Birthday December Live Mix - Part 2',
-        file: 'https://dj.david.pilato.fr/mixes/09_DJ_Dadoo_Anne_Birthday-December-Part2.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/09_DJ_Dadoo_Anne_Birthday-December-Part2.mp3',
         howl: null
     },
     {
         title: 'Summer Mix 2012',
-        file: 'https://dj.david.pilato.fr/mixes/08_DJ_Dadoo_Summer_Mix.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/08_DJ_Dadoo_Summer_Mix.mp3',
         howl: null
     },
     {
         title: 'July Live mix - Sebastien and Elvira Wedding - Part 1',
-        file: 'https://dj.david.pilato.fr/mixes/07_DJ_Dadoo_July-Mix-DDJT1-Part1.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/07_DJ_Dadoo_July-Mix-DDJT1-Part1.mp3',
         howl: null
     },
     {
         title: 'July Live mix - Sebastien and Elvira Wedding - Part 2',
-        file: 'https://dj.david.pilato.fr/mixes/07_DJ_Dadoo_July-Mix-DDJT1-Part2.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/07_DJ_Dadoo_July-Mix-DDJT1-Part2.mp3',
         howl: null
     },
     {
         title: 'May Mix 2012',
-        file: 'https://dj.david.pilato.fr/mixes/06_DJ_Dadoo_May-Mix-DDJT1.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/06_DJ_Dadoo_May-Mix-DDJT1.mp3',
         howl: null
     },
     {
         title: 'April Mix 2012',
-        file: 'https://dj.david.pilato.fr/mixes/05_DJ_Dadoo_April-Mix-DDJT1.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/05_DJ_Dadoo_April-Mix-DDJT1.mp3',
         howl: null
     },
     {
         title: 'March Mix 2012',
-        file: 'https://dj.david.pilato.fr/mixes/04_DJ_Dadoo_March-Mix-DDJT1.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/04_DJ_Dadoo_March-Mix-DDJT1.mp3',
         howl: null
     },
     {
         title: 'Club Mix 2012',
-        file: 'https://dj.david.pilato.fr/mixes/03_DJ_Dadoo_Club-Mix-DDJT1.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/03_DJ_Dadoo_Club-Mix-DDJT1.mp3',
         howl: null
     },
     {
         title: 'Funk Groove Mix 2012',
-        file: 'https://dj.david.pilato.fr/mixes/02_DJ_Dadoo_First-Mix-DDJT1.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/02_DJ_Dadoo_First-Mix-DDJT1.mp3',
         howl: null
     },
     {
         title: 'Funk Mix 2011',
-        file: 'https://dj.david.pilato.fr/mixes/01_DJ_Dadoo_Funk_Mix.mp3',
+        file: 'https://storage.googleapis.com/djdadoo/01_DJ_Dadoo_Funk_Mix.mp3',
         howl: null
     }
 ];
