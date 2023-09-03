55aa2691803e80acd52a2993cf93efc7e8337a69
David Pilato <david@pilato.fr>
2023-09-03 17:26:25 +0200

    Add anniversaire Caro et Nico

--- playlist.js
+++ playlist.js
@@ -1,167 +1,175 @@
 var dadoonetPlayList = [{
-        title: 'Funk / House Summer Mix 2023',
-        file: 'https://storage.googleapis.com/djdadoo/2023-08-25-HouseFunkSummerMix.mp3',
-        image: 'img/20230825-HouseFunkSummerMix.jpg'
-    },{
-        title: 'Le Camping des Speakers',
-        file: 'https://storage.googleapis.com/djdadoo/2023-06-14-camping-des-speakers.mp3',
-        image: 'img/20230613-CampingDesSpeakers.jpg'
-    },{
-        title: 'Heera\'s Birthday party',
-        file: 'https://storage.googleapis.com/djdadoo/2023-04-15-AnniversaireHeera.mp3',
-        image: 'img/20230415-Heera.jpg'
-    },{
-        title: 'Elastic France Private Event',
-        file: 'https://storage.googleapis.com/djdadoo/2023-03-09-ElasticPrivateEvent.mp3',
-        image: 'img/20230309-ElasticPrivateEvent.jpg'
-    },{
-        title: 'Studio Mix - Les Envahisseurs - Take 2',
-        file: 'https://storage.googleapis.com/djdadoo/2023-02-21-LesEnvahisseurs-Take2.mp3',
-        image: 'img/20230221-studio.jpg'
-    },{
-        title: 'Studio Mix - Snowcamp - Cergy',
-        file: 'https://storage.googleapis.com/djdadoo/2023-01-23-Snowcamp.mp3',
-        image: 'img/20230123-studio.jpg'
-    },{
-        title: 'Warmup - Meet Your Soul Mate - Paris',
-        file: 'https://storage.googleapis.com/djdadoo/2022-12-17-MeetYourSoulmate-Warmup.mp3',
-        image: 'img/20221217-meetyoursoulmate.jpg'
-    },{
-        title: 'Danse Part 1 - Meet Your Soul Mate - Paris',
-        file: 'https://storage.googleapis.com/djdadoo/2022-12-17-MeetYourSoulmate-Dance-Part1.mp3',
-        image: 'img/20221217-meetyoursoulmate.jpg'
-    },{
-        title: 'Danse Part 2 - Meet Your Soul Mate - Paris',
-        file: 'https://storage.googleapis.com/djdadoo/2022-12-17-MeetYourSoulmate-Dance-Part2.mp3',
-        image: 'img/20221217-meetyoursoulmate.jpg'
-    },{
-        title: 'Cocktail Mix - Anniversaire Gérard - Cergy',
-        file: 'https://storage.googleapis.com/djdadoo/2022-10-29-Anniversaire-Gerard-Part1.mp3',
-        image: 'img/20221029-Anniversaire-Gerard.jpg'
-    },{
-        title: 'Danse Part 1 - Anniversaire Gérard - Cergy',
-        file: 'https://storage.googleapis.com/djdadoo/2022-10-29-Anniversaire-Gerard-Part2.mp3',
-        image: 'img/20221029-Anniversaire-Gerard.jpg'
-    },{
-        title: 'Danse Part 2 - Anniversaire Gérard - Cergy',
-        file: 'https://storage.googleapis.com/djdadoo/2022-10-29-Anniversaire-Gerard-Part3.mp3',
-        image: 'img/20221029-Anniversaire-Gerard.jpg'
-    },{
-        title: 'Closing Mix - Anniversaire Gérard - Cergy',
-        file: 'https://storage.googleapis.com/djdadoo/2022-10-29-Anniversaire-Gerard-Part4.mp3',
-        image: 'img/20221029-Anniversaire-Gerard.jpg'
-    },{
-        title: 'Cocktail Mix - Wedding - Sucy en Brie',
-        file: 'https://storage.googleapis.com/djdadoo/2022-07-09-Wedding-Cocktail-Sucy-En-Brie.mp3',
-        image: 'img/20220709-cocktail-mix.jpg'
-    }, {
-        title: '1er An - Lolo et Mika',
-        file: 'https://storage.googleapis.com/djdadoo/2020-01-01-LoloEtMika-1An-fullmix.mp3',
-        image: 'img/20191231-1erAnLoloEtMika.jpg'
-    },
-    {
-        title: 'Wedding Natacha - Warm Up',
-        file: 'https://storage.googleapis.com/djdadoo/2019-08-31-Wedding-Natacha-Warmup.mp3',
-        image: 'img/20190831-WeddingNatacha.jpg'
-    },
-    {
-        title: 'BreizhCamp 2018 - Meet And Greet Party Mix (with Julien Ponge and David Pilato)',
-        file: 'https://storage.googleapis.com/djdadoo/2018-03-29-breizhcamp-meet-and-greet.mp3',
-        image: 'img/20180329-breizhcamp-meet-and-greet.jpg'
-    },
-    {
-        title: 'BreizhCamp 2017 - Meet And Greet Party Mix',
-        file: 'https://storage.googleapis.com/djdadoo/2017-04-20-breizhcamp-meet-and-greet.mp3',
-        image: 'img/20170420-breizhcamp-meet-and-greet.jpg'
-    },
-    {
-        title: 'BreizhCamp 2017 - Keynote Mix',
-        file: 'https://storage.googleapis.com/djdadoo/2017-04-20-breizhcamp-keynote.mp3',
-        image: 'img/20170420-breizhcamp-keynote.jpg'
-    },
-    {
-        title: 'Winter 2017 Pre Elastic{ON} 17 Mix',
-        file: 'https://storage.googleapis.com/djdadoo/2017-03-05-Winter-2017-Pre-ElasticON-Mix.mp3'
-    },
-    {
-        title: 'Summer 2016 Garden Mix',
-        file: 'https://storage.googleapis.com/djdadoo/2016-08-28-Summer-2016-Garden-Mix.mp3',
-        image: 'img/20160828-garden-mix.jpg'
-    },
-    {
-        title: 'Live - Fred Birthday',
-        file: 'https://storage.googleapis.com/djdadoo/2016-07-02-Fred-Birthday-Cergy.mp3'
-    },
-    {
-        title: 'Pre elastic{ON} 16 Mix',
-        file: 'https://storage.googleapis.com/djdadoo/2016-02-12-Pre-ElasticON16-Mix.mp3'
-    },
-    {
-        title: 'Fred Birthday Live Mix - Part 1',
-        file: 'https://storage.googleapis.com/djdadoo/2015-11-21-Fred-Birthday-Live-Mix-Part1.mp3'
-    },
-    {
-        title: 'Fred Birthday Live Mix - Part 2',
-        file: 'https://storage.googleapis.com/djdadoo/2015-11-21-Fred-Birthday-Live-Mix-Part2.mp3'
-    },
-    {
-        title: 'Pre elastic{ON} 15 Mix',
-        file: 'https://storage.googleapis.com/djdadoo/2015-01-18-pre-elasticon.mp3'
-    },
-    {
-        title: 'October Peniche Mix - Diner',
-        file: 'https://storage.googleapis.com/djdadoo/2013-10-05-Peniche-Diner.mp3'
-    },
-    {
-        title: 'October Peniche Mix - Dance Part 1 (80s)',
-        file: 'https://storage.googleapis.com/djdadoo/2013-10-05-Peniche-80.mp3'
-    },
-    {
-        title: 'October Peniche Mix - Dance Part 2 (Club)',
-        file: 'https://storage.googleapis.com/djdadoo/2013-10-05-Peniche-Dance.mp3'
-    },
-    {
-        title: 'Anne s Birthday December Live Mix - Part 1',
-        file: 'https://storage.googleapis.com/djdadoo/09_DJ_Dadoo_Anne_Birthday-December-Part1.mp3'
-    },
-    {
-        title: 'Anne s Birthday December Live Mix - Part 2',
-        file: 'https://storage.googleapis.com/djdadoo/09_DJ_Dadoo_Anne_Birthday-December-Part2.mp3'
-    },
-    {
-        title: 'Summer Mix 2012',
-        file: 'https://storage.googleapis.com/djdadoo/08_DJ_Dadoo_Summer_Mix.mp3'
-    },
-    {
-        title: 'July Live mix - Sebastien and Elvira Wedding - Part 1',
-        file: 'https://storage.googleapis.com/djdadoo/07_DJ_Dadoo_July-Mix-DDJT1-Part1.mp3'
-    },
-    {
-        title: 'July Live mix - Sebastien and Elvira Wedding - Part 2',
-        file: 'https://storage.googleapis.com/djdadoo/07_DJ_Dadoo_July-Mix-DDJT1-Part2.mp3'
-    },
-    {
-        title: 'May Mix 2012',
-        file: 'https://storage.googleapis.com/djdadoo/06_DJ_Dadoo_May-Mix-DDJT1.mp3'
-    },
-    {
-        title: 'April Mix 2012',
-        file: 'https://storage.googleapis.com/djdadoo/05_DJ_Dadoo_April-Mix-DDJT1.mp3'
-    },
-    {
-        title: 'March Mix 2012',
-        file: 'https://storage.googleapis.com/djdadoo/04_DJ_Dadoo_March-Mix-DDJT1.mp3'
-    },
-    {
-        title: 'Club Mix 2012',
-        file: 'https://storage.googleapis.com/djdadoo/03_DJ_Dadoo_Club-Mix-DDJT1.mp3'
-    },
-    {
-        title: 'Funk Groove Mix 2012',
-        file: 'https://storage.googleapis.com/djdadoo/02_DJ_Dadoo_First-Mix-DDJT1.mp3'
-    },
-    {
-        title: 'Funk Mix 2011',
-        file: 'https://storage.googleapis.com/djdadoo/01_DJ_Dadoo_Funk_Mix.mp3'
-    }
+    title: 'Anniversaire Caroline et Nicolas 50 ans - Part 2',
+    file: 'https://storage.googleapis.com/djdadoo/2023-09-03-AnnivCaroNicoPart2.mp3',
+    image: 'img/20230902-anniv-caronico.jpg'
+},{
+    title: 'Anniversaire Caroline et Nicolas 50 ans - Part 1',
+    file: 'https://storage.googleapis.com/djdadoo/2023-09-02-AnnivCaroNicoPart1.mp3',
+    image: 'img/20230902-anniv-caronico.jpg'
+},{
+    title: 'Funk / House Summer Mix 2023',
+    file: 'https://storage.googleapis.com/djdadoo/2023-08-25-HouseFunkSummerMix.mp3',
+    image: 'img/20230825-HouseFunkSummerMix.jpg'
+},{
+    title: 'Le Camping des Speakers',
+    file: 'https://storage.googleapis.com/djdadoo/2023-06-14-camping-des-speakers.mp3',
+    image: 'img/20230613-CampingDesSpeakers.jpg'
+},{
+    title: 'Heera\'s Birthday party',
+    file: 'https://storage.googleapis.com/djdadoo/2023-04-15-AnniversaireHeera.mp3',
+    image: 'img/20230415-Heera.jpg'
+},{
+    title: 'Elastic France Private Event',
+    file: 'https://storage.googleapis.com/djdadoo/2023-03-09-ElasticPrivateEvent.mp3',
+    image: 'img/20230309-ElasticPrivateEvent.jpg'
+},{
+    title: 'Studio Mix - Les Envahisseurs - Take 2',
+    file: 'https://storage.googleapis.com/djdadoo/2023-02-21-LesEnvahisseurs-Take2.mp3',
+    image: 'img/20230221-studio.jpg'
+},{
+    title: 'Studio Mix - Snowcamp - Cergy',
+    file: 'https://storage.googleapis.com/djdadoo/2023-01-23-Snowcamp.mp3',
+    image: 'img/20230123-studio.jpg'
+},{
+    title: 'Warmup - Meet Your Soul Mate - Paris',
+    file: 'https://storage.googleapis.com/djdadoo/2022-12-17-MeetYourSoulmate-Warmup.mp3',
+    image: 'img/20221217-meetyoursoulmate.jpg'
+},{
+    title: 'Danse Part 1 - Meet Your Soul Mate - Paris',
+    file: 'https://storage.googleapis.com/djdadoo/2022-12-17-MeetYourSoulmate-Dance-Part1.mp3',
+    image: 'img/20221217-meetyoursoulmate.jpg'
+},{
+    title: 'Danse Part 2 - Meet Your Soul Mate - Paris',
+    file: 'https://storage.googleapis.com/djdadoo/2022-12-17-MeetYourSoulmate-Dance-Part2.mp3',
+    image: 'img/20221217-meetyoursoulmate.jpg'
+},{
+    title: 'Cocktail Mix - Anniversaire Gérard - Cergy',
+    file: 'https://storage.googleapis.com/djdadoo/2022-10-29-Anniversaire-Gerard-Part1.mp3',
+    image: 'img/20221029-Anniversaire-Gerard.jpg'
+},{
+    title: 'Danse Part 1 - Anniversaire Gérard - Cergy',
+    file: 'https://storage.googleapis.com/djdadoo/2022-10-29-Anniversaire-Gerard-Part2.mp3',
+    image: 'img/20221029-Anniversaire-Gerard.jpg'
+},{
+    title: 'Danse Part 2 - Anniversaire Gérard - Cergy',
+    file: 'https://storage.googleapis.com/djdadoo/2022-10-29-Anniversaire-Gerard-Part3.mp3',
+    image: 'img/20221029-Anniversaire-Gerard.jpg'
+},{
+    title: 'Closing Mix - Anniversaire Gérard - Cergy',
+    file: 'https://storage.googleapis.com/djdadoo/2022-10-29-Anniversaire-Gerard-Part4.mp3',
+    image: 'img/20221029-Anniversaire-Gerard.jpg'
+},{
+    title: 'Cocktail Mix - Wedding - Sucy en Brie',
+    file: 'https://storage.googleapis.com/djdadoo/2022-07-09-Wedding-Cocktail-Sucy-En-Brie.mp3',
+    image: 'img/20220709-cocktail-mix.jpg'
+}, {
+    title: '1er An - Lolo et Mika',
+    file: 'https://storage.googleapis.com/djdadoo/2020-01-01-LoloEtMika-1An-fullmix.mp3',
+    image: 'img/20191231-1erAnLoloEtMika.jpg'
+},
+{
+    title: 'Wedding Natacha - Warm Up',
+    file: 'https://storage.googleapis.com/djdadoo/2019-08-31-Wedding-Natacha-Warmup.mp3',
+    image: 'img/20190831-WeddingNatacha.jpg'
+},
+{
+    title: 'BreizhCamp 2018 - Meet And Greet Party Mix (with Julien Ponge and David Pilato)',
+    file: 'https://storage.googleapis.com/djdadoo/2018-03-29-breizhcamp-meet-and-greet.mp3',
+    image: 'img/20180329-breizhcamp-meet-and-greet.jpg'
+},
+{
+    title: 'BreizhCamp 2017 - Meet And Greet Party Mix',
+    file: 'https://storage.googleapis.com/djdadoo/2017-04-20-breizhcamp-meet-and-greet.mp3',
+    image: 'img/20170420-breizhcamp-meet-and-greet.jpg'
+},
+{
+    title: 'BreizhCamp 2017 - Keynote Mix',
+    file: 'https://storage.googleapis.com/djdadoo/2017-04-20-breizhcamp-keynote.mp3',
+    image: 'img/20170420-breizhcamp-keynote.jpg'
+},
+{
+    title: 'Winter 2017 Pre Elastic{ON} 17 Mix',
+    file: 'https://storage.googleapis.com/djdadoo/2017-03-05-Winter-2017-Pre-ElasticON-Mix.mp3'
+},
+{
+    title: 'Summer 2016 Garden Mix',
+    file: 'https://storage.googleapis.com/djdadoo/2016-08-28-Summer-2016-Garden-Mix.mp3',
+    image: 'img/20160828-garden-mix.jpg'
+},
+{
+    title: 'Live - Fred Birthday',
+    file: 'https://storage.googleapis.com/djdadoo/2016-07-02-Fred-Birthday-Cergy.mp3'
+},
+{
+    title: 'Pre elastic{ON} 16 Mix',
+    file: 'https://storage.googleapis.com/djdadoo/2016-02-12-Pre-ElasticON16-Mix.mp3'
+},
+{
+    title: 'Fred Birthday Live Mix - Part 1',
+    file: 'https://storage.googleapis.com/djdadoo/2015-11-21-Fred-Birthday-Live-Mix-Part1.mp3'
+},
+{
+    title: 'Fred Birthday Live Mix - Part 2',
+    file: 'https://storage.googleapis.com/djdadoo/2015-11-21-Fred-Birthday-Live-Mix-Part2.mp3'
+},
+{
+    title: 'Pre elastic{ON} 15 Mix',
+    file: 'https://storage.googleapis.com/djdadoo/2015-01-18-pre-elasticon.mp3'
+},
+{
+    title: 'October Peniche Mix - Diner',
+    file: 'https://storage.googleapis.com/djdadoo/2013-10-05-Peniche-Diner.mp3'
+},
+{
+    title: 'October Peniche Mix - Dance Part 1 (80s)',
+    file: 'https://storage.googleapis.com/djdadoo/2013-10-05-Peniche-80.mp3'
+},
+{
+    title: 'October Peniche Mix - Dance Part 2 (Club)',
+    file: 'https://storage.googleapis.com/djdadoo/2013-10-05-Peniche-Dance.mp3'
+},
+{
+    title: 'Anne s Birthday December Live Mix - Part 1',
+    file: 'https://storage.googleapis.com/djdadoo/09_DJ_Dadoo_Anne_Birthday-December-Part1.mp3'
+},
+{
+    title: 'Anne s Birthday December Live Mix - Part 2',
+    file: 'https://storage.googleapis.com/djdadoo/09_DJ_Dadoo_Anne_Birthday-December-Part2.mp3'
+},
+{
+    title: 'Summer Mix 2012',
+    file: 'https://storage.googleapis.com/djdadoo/08_DJ_Dadoo_Summer_Mix.mp3'
+},
+{
+    title: 'July Live mix - Sebastien and Elvira Wedding - Part 1',
+    file: 'https://storage.googleapis.com/djdadoo/07_DJ_Dadoo_July-Mix-DDJT1-Part1.mp3'
+},
+{
+    title: 'July Live mix - Sebastien and Elvira Wedding - Part 2',
+    file: 'https://storage.googleapis.com/djdadoo/07_DJ_Dadoo_July-Mix-DDJT1-Part2.mp3'
+},
+{
+    title: 'May Mix 2012',
+    file: 'https://storage.googleapis.com/djdadoo/06_DJ_Dadoo_May-Mix-DDJT1.mp3'
+},
+{
+    title: 'April Mix 2012',
+    file: 'https://storage.googleapis.com/djdadoo/05_DJ_Dadoo_April-Mix-DDJT1.mp3'
+},
+{
+    title: 'March Mix 2012',
+    file: 'https://storage.googleapis.com/djdadoo/04_DJ_Dadoo_March-Mix-DDJT1.mp3'
+},
+{
+    title: 'Club Mix 2012',
+    file: 'https://storage.googleapis.com/djdadoo/03_DJ_Dadoo_Club-Mix-DDJT1.mp3'
+},
+{
+    title: 'Funk Groove Mix 2012',
+    file: 'https://storage.googleapis.com/djdadoo/02_DJ_Dadoo_First-Mix-DDJT1.mp3'
+},
+{
+    title: 'Funk Mix 2011',
+    file: 'https://storage.googleapis.com/djdadoo/01_DJ_Dadoo_Funk_Mix.mp3'
+}
 ];
